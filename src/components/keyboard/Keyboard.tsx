2b30c5d2756657563e0a3d087184822b0a956c4a
Tomas Liubinas <git@liubinas.com>
2022-07-16 15:19:15 +0300

    Localization

--- src/components/keyboard/Keyboard.tsx
+++ src/components/keyboard/Keyboard.tsx
@@ -14,13 +14,13 @@ type Props = {
 }
 
 export const Keyboard = ({
-  onChar,
-  onDelete,
-  onEnter,
-  solution,
-  guesses,
-  isRevealing,
-}: Props) => {
+                           onChar,
+                           onDelete,
+                           onEnter,
+                           solution,
+                           guesses,
+                           isRevealing,
+                         }: Props) => {
   const charStatuses = getStatuses(solution, guesses)
 
   const onClick = (value: string) => {
@@ -54,46 +54,47 @@ export const Keyboard = ({
   }, [onEnter, onDelete, onChar])
 
   return (
-    <div>
-      <div className="flex justify-center mb-1">
-        {['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P'].map((key) => (
-          <Key
-            value={key}
-            key={key}
-            onClick={onClick}
-            status={charStatuses[key]}
-            isRevealing={isRevealing}
-          />
-        ))}
+      <div>
+        <div className="flex justify-center mb-1">
+          {['Ą', 'Ž', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P',  'Ė'].map((key) => (
+              <Key
+                  value={key}
+                  key={key}
+                  onClick={onClick}
+                  status={charStatuses[key]}
+                  isRevealing={isRevealing}
+              />
+          ))}
+          <Key width={65.4} value="DELETE" onClick={onClick}>
+            {DELETE_TEXT}
+          </Key>
+        </div>
+        <div className="flex justify-center mb-1">
+          {['A', 'S', 'Š', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'Ų'].map((key) => (
+              <Key
+                  value={key}
+                  key={key}
+                  onClick={onClick}
+                  status={charStatuses[key]}
+                  isRevealing={isRevealing}
+              />
+          ))}
+        </div>
+        <div className="flex justify-center">
+
+          {['Z', 'Ū', 'C', 'V', 'B', 'N', 'M', 'Č', 'Ę'].map((key) => (
+              <Key
+                  value={key}
+                  key={key}
+                  onClick={onClick}
+                  status={charStatuses[key]}
+                  isRevealing={isRevealing}
+              />
+          ))}
+          <Key width={120} value="ENTER" onClick={onClick}>
+            {ENTER_TEXT}
+          </Key>
+        </div>
       </div>
-      <div className="flex justify-center mb-1">
-        {['A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L'].map((key) => (
-          <Key
-            value={key}
-            key={key}
-            onClick={onClick}
-            status={charStatuses[key]}
-            isRevealing={isRevealing}
-          />
-        ))}
-      </div>
-      <div className="flex justify-center">
-        <Key width={65.4} value="ENTER" onClick={onClick}>
-          {ENTER_TEXT}
-        </Key>
-        {['Z', 'X', 'C', 'V', 'B', 'N', 'M'].map((key) => (
-          <Key
-            value={key}
-            key={key}
-            onClick={onClick}
-            status={charStatuses[key]}
-            isRevealing={isRevealing}
-          />
-        ))}
-        <Key width={65.4} value="DELETE" onClick={onClick}>
-          {DELETE_TEXT}
-        </Key>
-      </div>
-    </div>
   )
 }
--- src/constants/strings.ts
+++ src/constants/strings.ts
@@ -1,32 +1,32 @@
 export const GAME_TITLE = process.env.REACT_APP_GAME_NAME!
 
-export const WIN_MESSAGES = ['Great Job!', 'Awesome', 'Well done!']
-export const GAME_COPIED_MESSAGE = 'Game copied to clipboard'
-export const NOT_ENOUGH_LETTERS_MESSAGE = 'Not enough letters'
-export const WORD_NOT_FOUND_MESSAGE = 'Word not found'
+export const WIN_MESSAGES = ['Puiku!', 'Sveikiname!', 'Šaunu!']
+export const GAME_COPIED_MESSAGE = 'Žaidimo duomenys nukopijuoti į iškarpinę'
+export const NOT_ENOUGH_LETTERS_MESSAGE = 'Per mažai raidžių'
+export const WORD_NOT_FOUND_MESSAGE = 'Žodis nerastas'
 export const HARD_MODE_ALERT_MESSAGE =
-  'Hard Mode can only be enabled at the start!'
+    'Pasunkinimas gali būti pasirinktas prieš žaidimą!'
 export const HARD_MODE_DESCRIPTION =
-  'Any revealed hints must be used in subsequent guesses'
-export const HIGH_CONTRAST_MODE_DESCRIPTION = 'For improved color vision'
+    'Visos atverstos užuominos turi būti panaudotos'
+export const HIGH_CONTRAST_MODE_DESCRIPTION = 'Kontrastingos spalvos'
 export const CORRECT_WORD_MESSAGE = (solution: string) =>
-  `The word was ${solution}`
+    `Parinktas žodis buvo ${solution}`
 export const WRONG_SPOT_MESSAGE = (guess: string, position: number) =>
-  `Must use ${guess} in position ${position}`
+    `${guess} turi būti panaudota ${position} pozicijoje`
 export const NOT_CONTAINED_MESSAGE = (letter: string) =>
-  `Guess must contain ${letter}`
-export const ENTER_TEXT = 'Enter'
-export const DELETE_TEXT = 'Delete'
-export const STATISTICS_TITLE = 'Statistics'
-export const GUESS_DISTRIBUTION_TEXT = 'Guess Distribution'
-export const NEW_WORD_TEXT = 'New word in'
-export const SHARE_TEXT = 'Share'
-export const MIGRATE_BUTTON_TEXT = 'Transfer'
+    `Spėjime privalu įtraukti ${letter}`
+export const ENTER_TEXT = 'PATEIKTI'
+export const DELETE_TEXT = '⌫'
+export const STATISTICS_TITLE = 'Pasiekimai'
+export const GUESS_DISTRIBUTION_TEXT = 'Sėkmingi spėjimai'
+export const NEW_WORD_TEXT = 'Naujas žodis už'
+export const SHARE_TEXT = 'Dalintis'
+export const MIGRATE_BUTTON_TEXT = 'Perkelti pasiekimus'
 export const MIGRATE_DESCRIPTION_TEXT =
-  'Click here to transfer your statistics to a new device.'
-export const TOTAL_TRIES_TEXT = 'Total tries'
-export const SUCCESS_RATE_TEXT = 'Success rate'
-export const CURRENT_STREAK_TEXT = 'Current streak'
-export const BEST_STREAK_TEXT = 'Best streak'
+    'Paspauskite norėdami perkelti savo pasiekimus į kitą įrenginį'
+export const TOTAL_TRIES_TEXT = 'Sužaista'
+export const SUCCESS_RATE_TEXT = 'Sėkmės'
+export const CURRENT_STREAK_TEXT = 'Atspėta iš eilės'
+export const BEST_STREAK_TEXT = 'Ilgiausia eilė'
 export const DISCOURAGE_INAPP_BROWSER_TEXT =
-  "You are using an embedded browser and may experience problems sharing or saving your results. We encourage you rather to use your device's default browser."
+    "You are using an embedded browser and may experience problems sharing or saving your results. We encourage you rather to use your device's default browser."
