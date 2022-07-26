b0bd2d71ef79e99ab55b4da5aefa81df0d63ff0b
Tomas Liubinas <git@liubinas.com>
2022-07-26 13:44:39 +0300

    Clear rules

--- src/components/modals/InfoModal.tsx
+++ src/components/modals/InfoModal.tsx
@@ -10,10 +10,15 @@ export const InfoModal = ({ isOpen, handleClose }: Props) => {
   return (
     <BaseModal title="Žaidimo taisyklės" isOpen={isOpen} handleClose={handleClose}>
       <p className="text-sm text-gray-500 dark:text-gray-300">
-        Atspėkite žodį iš 6 spėjimų. Po kiekvieno spėjimo raidžių spalva parodys ar priartėjote prie tikslo.
+        Atspėkite parinktą žodį iš 6 spėjimų.
       </p>
 
-      <div className="flex justify-center mb-1 mt-4">
+        <p className="text-sm text-gray-500 dark:text-gray-300">
+            Po kiekvieno spėjimo raidžių spalva parodys ar priartėjote prie tikslo.
+        </p>
+
+
+        <div className="flex justify-center mb-1 mt-4">
         <Cell
           isRevealing={true}
           isCompleted={true}
@@ -21,7 +26,9 @@ export const InfoModal = ({ isOpen, handleClose }: Props) => {
           status="correct"
         />
         <Cell isRevealing={true} isCompleted={true}  status="absent"  value="I" />
-        <Cell isRevealing={true} isCompleted={true}  status="absent"  value="N" />
+        <Cell isRevealing={true}
+              isCompleted={true} status="present"
+              value="N" />
         <Cell isRevealing={true} isCompleted={true}  status="absent"  value="I" />
         <Cell isRevealing={true} isCompleted={true}  status="absent"  value="A" />
       </div>
@@ -29,21 +36,13 @@ export const InfoModal = ({ isOpen, handleClose }: Props) => {
         Raidė Ž yra žodyje teisingoje vietoje
       </p>
 
-      <div className="flex justify-center mb-1 mt-4">
-        <Cell isRevealing={true} isCompleted={true}  status="absent" value="M" />
-        <Cell isRevealing={true} isCompleted={true}  status="absent" value="Ū" />
-        <Cell
-          isRevealing={true}
-          isCompleted={true}
-          value="R"
-          status="present"
-        />
-        <Cell isRevealing={true} isCompleted={true}  status="absent" value="A" />
-        <Cell isRevealing={true} isCompleted={true}  status="absent" value="S" />
-      </div>
       <p className="text-sm text-gray-500 dark:text-gray-300">
-        Raidė R yra žodyje, bet kitoje vietoje
+        Raidė N yra žodyje, bet kitoje vietoje
       </p>
+
+        <p className="text-sm text-gray-500 dark:text-gray-300">
+            Kitų raidžių parinktame žodyje nėra
+        </p>
 
 
       <p className="mt-6 italic text-sm text-gray-500 dark:text-gray-300">
@@ -52,8 +51,12 @@ export const InfoModal = ({ isOpen, handleClose }: Props) => {
           href="https://github.com/tomasliubinas/react-wordle"
           className="underline font-bold"
         >
-          React Wordle yra atvirojo kodo žaidimo versija
+          Tai yra atvirojo kodo žaidimo Wordle versija
         </a>{' '}
+
+          <p className="text-sm text-gray-500 dark:text-gray-300">
+              Įgyvendino T. Liubinas
+          </p>
 
       </p>
     </BaseModal>
